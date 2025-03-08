640037975a08f6f2117327c679fed74f1574cd25
Deepak <deepakviji5556@gmail.com>
2025-03-08 13:43:12 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const port = 7125;
+
+
+app.get('/',(req,res) => {
+ res.json({message: 'Hello World!'});
+});
+
+// app.listen(port,()=>{
+//     console.log("port",port);
+// })
+//or
+app.listen(port,()=>{
+    console.log(`port:${port}`);
+})
---
+++ routes/movies.route.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+// CRUD Operation
+
+// C
+
+// R
+
+// U
+
+// D
